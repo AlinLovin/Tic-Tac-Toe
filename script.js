d8efc0892c7c16e6b7080cf49656d71d3a372008
AlinLovin <78381461+AlinLovin@users.noreply.github.com>
2021-04-18 20:08:48 +0300

    Add code

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
+var arrayGrid = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
+var currentPlayer = 'X';
 
+window.onload = function() {
+	var game = document.getElementById('xoId');
+	for (let row = 0; row < 3; ++row) {
+		for (let column = 0; column < 3; ++column) {
+			game.innerHTML += '<div class="cell" id="' + row + column + '" onclick="play(' + row + ', ' + column + ')"> &nbsp </div>';
+		}
+	}
+}
+
+function play(i , j) {
+	if (arrayGrid[i][j] == ' ') {
+		arrayGrid[i][j] = currentPlayer;
+		if (currentPlayer == 'X') {
+			document.getElementById('' + i + j).style.backgroundColor = "#ff3333";
+		} else {
+			document.getElementById('' + i + j).style.backgroundColor = "lightblue";
+		}
+
+		if (currentPlayer == 'X') {
+			currentPlayer = 'O';
+		} else {
+			currentPlayer = 'X';
+		}
+		
+		document.getElementById('' + i + j).innerHTML = arrayGrid[i][j];
+		gameState(i , j);
+	}
+}
+
+function gameState(row, col) {
+	var player = arrayGrid[row][col];
+	var isDraw = 1;
+
+	for (let i = 0; i < 3; ++i) {
+		for (let j = 0; j < 3; ++j) {
+			if (arrayGrid[i][0] == player && arrayGrid[i][1] == player && arrayGrid[i][2] == player) {
+				document.getElementById("winner").innerHTML = "The winner is " + player; 
+			}
+			if (arrayGrid[0][i] == player && arrayGrid[1][i] == player && arrayGrid[2][i] == player) {
+				document.getElementById("winner").innerHTML = "The winner is " + player; 
+			}
+			if (arrayGrid[i][j] == ' ') {
+				isDraw = 0;
+			}
+		}
+	}
+
+	if (isDraw == 1) {
+		document.getElementById("winner").innerHTML = "The game ended in a draw.";
+	}
+	if (arrayGrid[0][0] == player && arrayGrid[1][1] == player && arrayGrid[2][2] == player) {
+		document.getElementById("winner").innerHTML = "The winner is " + player; 
+	}
+	if (arrayGrid[0][2] == player && arrayGrid[1][1] == player && arrayGrid[2][0] == player) {
+		document.getElementById("winner").innerHTML = "The winner is " + player; 
+	}
+}
