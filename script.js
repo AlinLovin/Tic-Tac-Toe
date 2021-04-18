25b738c913e80fe8a6e5c9fd6631a0f329bffb80
AlinLovin <78381461+AlinLovin@users.noreply.github.com>
2021-04-18 20:34:55 +0300

    Complete project

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var arrayGrid = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
 var currentPlayer = 'X';
 
 window.onload = function() {
-	var game = document.getElementById('xoId');
+	var game = document.getElementById('grid');
 	for (let row = 0; row < 3; ++row) {
 		for (let column = 0; column < 3; ++column) {
 			game.innerHTML += '<div class="cell" id="' + row + column + '" onclick="play(' + row + ', ' + column + ')"> &nbsp </div>';
@@ -33,14 +33,17 @@ function play(i , j) {
 function gameState(row, col) {
 	var player = arrayGrid[row][col];
 	var isDraw = 1;
+	var win = 0;
 
 	for (let i = 0; i < 3; ++i) {
 		for (let j = 0; j < 3; ++j) {
 			if (arrayGrid[i][0] == player && arrayGrid[i][1] == player && arrayGrid[i][2] == player) {
-				document.getElementById("winner").innerHTML = "The winner is " + player; 
+				document.getElementById("final").innerHTML = "The winner is " + player; 
+				win = 1;
 			}
 			if (arrayGrid[0][i] == player && arrayGrid[1][i] == player && arrayGrid[2][i] == player) {
-				document.getElementById("winner").innerHTML = "The winner is " + player; 
+				document.getElementById("final").innerHTML = "The winner is " + player; 
+				win = 1;
 			}
 			if (arrayGrid[i][j] == ' ') {
 				isDraw = 0;
@@ -48,13 +51,11 @@ function gameState(row, col) {
 		}
 	}
 
-	if (isDraw == 1) {
-		document.getElementById("winner").innerHTML = "The game ended in a draw.";
-	}
 	if (arrayGrid[0][0] == player && arrayGrid[1][1] == player && arrayGrid[2][2] == player) {
-		document.getElementById("winner").innerHTML = "The winner is " + player; 
-	}
-	if (arrayGrid[0][2] == player && arrayGrid[1][1] == player && arrayGrid[2][0] == player) {
-		document.getElementById("winner").innerHTML = "The winner is " + player; 
+		document.getElementById("final").innerHTML = "The winner is " + player;
+	} else if (arrayGrid[0][2] == player && arrayGrid[1][1] == player && arrayGrid[2][0] == player) {
+		document.getElementById("final").innerHTML = "The winner is " + player; 
+	} else if (isDraw == 1 && win == 0) {
+		document.getElementById("final").innerHTML = "The game ended in a draw.";
 	}
 }
